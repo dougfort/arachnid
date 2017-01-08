02a58fc8e937c5db3c29af6eecbf466c3db60d76
dougfort <doug.fort@gmail.com>
2017-01-08 15:15:48 -0500

    do the easy parts of parseCommand

--- src/ParseCommand.hs
+++ src/ParseCommand.hs
@@ -4,7 +4,18 @@ module ParseCommand
     ) where
 
 data Command = Quit
+             | New
+             | Deal
+             | Error String
   deriving (Eq, Show)
 
 parseCommand :: String -> Command
-parseCommand _ = Quit
+parseCommand line =
+  case words line of
+    ["quit"]  -> Quit
+    ["new"]   -> New
+    ["deal"]  -> Deal
+    "move":_  -> Error "move isn't written yet"
+    "eat":[_] -> Error "eat isn't written yet"
+    ["scan"]  -> Error "scan isn't written yet"
+    _         -> Error ("unknown command '" ++ line ++ "'")
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,12 @@ import           Test.Hspec
 
 main :: IO ()
 main = hspec $
-  describe "Prelude.read" $
+  describe "Prelude.read" $ do
     it "can parse 'quit'" $
       parseCommand "quit" `shouldBe` Quit
+
+    it "can parse 'new'" $
+      parseCommand "new" `shouldBe` New
+
+    it "can parse 'deal'" $
+      parseCommand "deal" `shouldBe` Deal
